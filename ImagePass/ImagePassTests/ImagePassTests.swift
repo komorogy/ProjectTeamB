827bf610d6ca7caa2a0388db5f3fe7804a406728
log <logk0910036@gmail.com>
2015-09-16 15:45:18 +0900

    ImagePass_Ver1.0

---
+++ ImagePass/ImagePassTests/ImagePassTests.swift
@@ -0,0 +1,36 @@
+//
+//  ImagePassTests.swift
+//  ImagePassTests
+//
+//  Created by 川述　優 on 2015/09/16.
+//  Copyright (c) 2015年 川述　優. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class ImagePassTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
