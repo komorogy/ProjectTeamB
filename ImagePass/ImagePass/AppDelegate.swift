685d5d07e494b98e18ab2c8aab27c28232258764
log <logk0910036@gmail.com>
2015-09-27 19:39:23 +0900

    no message

--- ImagePass/ImagePass/AppDelegate.swift
+++ ImagePass/ImagePass/AppDelegate.swift
@@ -13,6 +13,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    //画面間のパラメータ用の変数
+    let ID:String ;
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
