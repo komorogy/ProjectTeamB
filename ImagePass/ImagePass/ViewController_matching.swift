b797527e8cd3813d658e022ffbfaca041d5f1f9c
kou_sugimoto <sugimoto@orc-net.com>
2015-11-08 16:36:42 +0900

    位置情報取得処理、目標との比較処理

--- ImagePass/ImagePass/ViewController_matching.swift
+++ ImagePass/ImagePass/ViewController_matching.swift
@@ -8,16 +8,89 @@
 //
 
 import UIKit
-/*
-// ディレクトリをいじったりするクラス？みたいなの．defaultManager()でインスタンス化するみたい．
-private let fileManager:NSFileManager = NSFileManager.defaultManager() ;
-*/
+import CoreLocation
 
-class ViewController_matching: UIViewController {
+
+class ViewController_matching: UIViewController, CLLocationManagerDelegate  {
+    
+    var lm: CLLocationManager!
+    // 取得した緯度、keidoを保持するインスタンス
+    var latitude: CLLocationDegrees!
+    var longitude: CLLocationDegrees!
+    
+    // 目標地点
+    var goalLatitude: CLLocationDegrees!;
+    var goalLongtitude: CLLocationDegrees!;
+    
+    // GPS照合失敗回数
+    var errCnt: Int = 0;
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // var memo : objc_object!;// 指定されたメモのデータが入ってると想定
+        // var memo = { title: "メモのタイトル", text: "メモ本文", latitude: 1369.9, longtitue: 35.8}
+        var memo = ["メモのタイトル", "本文", "136.9", "35.8"];
+        /**
+         * memo = { title: メモのタイトル, text: メモ本文, latitude: 緯度, longtitue: 経度}
+         * あるいはmemo = [メモのタイトル, 本文, 緯度, 経度];
+         */
+        
+        
+       
+        // 目標地点
+        //var goalLatitude = memo.latitude;
+        //var goalLongtitude = memo.longtitude;
+        goalLatitude = Double(memo[2]);
+        goalLongtitude = Double(memo[3]);
+        
+        // フィールドの初期化
+        lm = CLLocationManager()
+        longitude = CLLocationDegrees()
+        latitude = CLLocationDegrees()
+        
+        // CLLocationManagerをDelegateに指定
+        lm.delegate = self
+        
+        // 位置情報取得の許可を求めるメッセージの表示．必須．
+        lm.requestAlwaysAuthorization()
+        // 位置情報の精度を指定．任意，
+        lm.desiredAccuracy = kCLLocationAccuracyBest
+        // 位置情報取得間隔を指定．指定した値（メートル）移動したら位置情報を更新する．任意．
+        lm.distanceFilter = 15;
+        
+        // GPSの使用を開始する
+        lm.startUpdatingLocation()
+        lm.startUpdatingHeading()
+    }
+    
+    /* 位置情報取得成功時に実行される関数 */
+    func locationManager(manager: CLLocationManager, didUpdateToLocation newLocation: CLLocation, fromLocation oldLocation: CLLocation){
+        // 取得した緯度がnewLocation.coordinate.longitudeに格納されている
+        latitude = newLocation.coordinate.latitude
+        // 取得した経度がnewLocation.coordinate.longitudeに格納されている
+        longitude = newLocation.coordinate.longitude
+        
+        // 目標のGPSと現在地との差
+        var difLong = goalLongtitude - longitude;
+        var difLati = goalLatitude - latitude;
+        
+        if(difLong > -0.1 && difLong < 0.1 && difLati > -0.1 && difLati < 0.1){
+            NSLog("ok")
+            //GPSの使用を停止する．停止しない限りGPSは実行され，指定間隔で更新され続ける．
+            lm.stopUpdatingLocation()
+            // 画面遷移
+            
+        } else {
+            NSLog("ng")
+            errCnt++;
+        }
+
+    }
+
+    /*位置情報取得失敗時に実行される関数 */
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        NSLog(error.description);
     }
     
     override func didReceiveMemoryWarning() {
